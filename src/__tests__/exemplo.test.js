7a5b69297feed0890ed95a2b7e9dbd9a9fe29fd9
aquiles23 <jjoseaquiless@gmail.com>
2022-07-18 18:47:12 -0300

    49 instala dependencias cria um exemplo de teste

---
+++ src/__tests__/exemplo.test.js
@@ -0,0 +1,54 @@
+import renderer from 'react-test-renderer';
+import { useState } from 'react';
+
+// exemplo achado em https://jestjs.io/docs/tutorial-react
+function Link({ page, children }) {
+    const STATUS = {
+        HOVERED: 'hovered',
+        NORMAL: 'normal',
+    };
+    const [status, setStatus] = useState(STATUS.NORMAL);
+
+    const onMouseEnter = () => {
+        setStatus(STATUS.HOVERED);
+    };
+
+    const onMouseLeave = () => {
+        setStatus(STATUS.NORMAL);
+    };
+
+    return (
+        <a
+            className={status}
+            href={page || '#'}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+        >
+            {children}
+        </a>
+    );
+}
+
+test('changes the class when hovered', () => {
+    const component = renderer.create(
+        <Link page="http://www.facebook.com">Facebook</Link>,
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    // manually trigger the callback
+    renderer.act(() => {
+        tree.props.onMouseEnter();
+    });
+    // re-rendering
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+
+    // manually trigger the callback
+    renderer.act(() => {
+        tree.props.onMouseLeave();
+    });
+    // re-rendering
+    tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+});
