c105d96f075f806b75b61ce9281597cac12b7c74
Arthur Tavares <arthur001123@gmail.com>
2022-08-01 10:33:13 -0300

    Atualizando rota de serviço

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://ec2-3-80-150-123.compute-1.amazonaws.com:3333'
+  baseURL: 'https://capju-service.herokuapp.com/'
 });
 
 export default api;
