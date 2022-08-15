588d1d57246417b22d27cf28b74fa46339070a49
PabloChristianno <pablochristianno@gmail.com>
2022-08-14 23:27:44 -0300

    79-Implementando a função de buscar processos por registro ou apelido.
    Co-authored-by: Philipe Sousa <barrosphilipe07@gmail.com>
    Co-authored-by: Francisco Heronildo <heronildo.2010@hotmail.com>

--- src/pages/Processes/index.js
+++ src/pages/Processes/index.js
@@ -3,7 +3,6 @@ import { Container } from './styles';
 import { Link } from 'react-router-dom';
 import React from 'react';
 import api from '../../services/api';
-import TextInput from '../../components/TextInput';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import AddIcon from '@mui/icons-material/Add';
@@ -26,28 +25,23 @@ function Processes() {
     setProcesses(response.data.processes);
   }
 
+  //Catch the event when the input changes
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const renderProcessRegisterList = (arr) => {
-    return arr
-      .filter((processes) =>
-        processes.registro.toLowerCase().includes(searchTerm)
-      )
-      .map((processes) => {
-        return <p>{processes.registro}</p>;
-      });
-  };
-
-  const renderProcessNickList = (arr) => {
-    return arr
-      .filter((processes) =>
+  //Filter processes by register and nickname
+  const filterProcesses = (arr) => {
+    return arr.filter((processes) => {
+      if (searchTerm == '') {
+        return processes;
+      } else if (
+        processes.registro.toLowerCase().includes(searchTerm) ||
         processes.apelido.toLowerCase().includes(searchTerm)
-      )
-      .map((processes) => {
-        return <p>{processes.apelido}</p>;
-      });
+      ) {
+        return processes;
+      }
+    });
   };
 
   return (
@@ -55,16 +49,14 @@ function Processes() {
       <div className="processes">
         <h1>Processos</h1>
         <div className="processSearch">
-          <TextInput
+          <input
             value={searchTerm}
             placeholder="Buscar Processos"
             onChange={handleChange}
-          ></TextInput>
-          {renderProcessRegisterList(processes)}
-          {renderProcessNickList(processes)}
+          />
         </div>
         {processes.length == 0 && 'Nenhum processo foi encontrado'}
-        {processes.map((proc, idx) => {
+        {filterProcesses(processes).map((proc, idx) => {
           return (
             <div key={idx} className="process">
               {proc.apelido.length > 0
