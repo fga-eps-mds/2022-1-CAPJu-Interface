c103f210aa06daab037d2f093698f89de11b21ba
Natilorens <nathalialorenacardoso@gmail.com>
2022-08-17 23:57:20 -0300

    69-Melhorando a função de Login
    Co-Authored-by: Arthur <arthur001123@gmail.com>
    Co-Authored-by: Hellen <202016480@aluno.unb.br>

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,47 +3,56 @@ import { Container } from './styles';
 import { useState } from 'react';
 import TextInput from 'components/TextInput';
 import Button from 'components/Button';
-import { Content, Modal } from 'pages/Stages/styles';
-import axios from 'axios';
+import { Content } from 'pages/Stages/styles';
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import user from 'services/user';
 
 function Login() {
   const [newEmail, setEmail] = useState('');
   const [newPassword, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  async function login() {
+    const response = await user.post('/login', {
+      email: newEmail,
+      password: newPassword
+    });
+    if (response.status == 200) {
+      if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data));
+      }
+      toast.success('Usuário logado com  sucesso');
+      navigate('stages');
+    } else {
+      toast.error('Erro no login: ' + response.data?.message);
+    }
+  }
+
   return (
     <Container>
-      <Modal>
-        <Content>
-          <h1>Login</h1>
-          <TextInput
-            set={setEmail}
-            value={newEmail}
-            placeholder="Email"
-          ></TextInput>
-          <TextInput
-            set={setPassword}
-            value={newPassword}
-            placeholder="Senha"
-          ></TextInput>
-          <Button
-            onClick={async () => {
-              const response = await axios.post('http://localhost:3334/login', {
-                email: newEmail,
-                password: newPassword
-              });
-
-              if (response.data) {
-                localStorage.setItem('user', JSON.stringify(response.data));
-              }
-
-              return response.data;
-            }}
-          >
-            Entrar
-          </Button>
-        </Content>
-      </Modal>
+      <Content>
+        <h1>Login</h1>
+        <TextInput
+          set={setEmail}
+          value={newEmail}
+          placeholder="Email"
+        ></TextInput>
+        <TextInput
+          set={setPassword}
+          value={newPassword}
+          placeholder="Senha"
+        ></TextInput>
+        <Button
+          onClick={async () => {
+            login();
+          }}
+        >
+          Entrar
+        </Button>
+      </Content>
     </Container>
   );
 }
-
 export default Login;
