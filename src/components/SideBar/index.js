c978e1fc7b0ecf479e4df89b62b2a0fd59168b61
luclopesr <luclopesr@gmail.com>
2022-08-19 17:24:33 -0300

    68 - Incluindo ícone de processos na sideBar
    Signed-off-by: luclopesr <luclopesr@gmail.com>
    Co-authored-by: aquiles23 <jjoseaquiless@gmail.com>

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Container, MenuItem } from './styles';
 import { Flow } from '@styled-icons/fluentui-system-regular';
 import { FlowCascade } from '@styled-icons/entypo';
+import { ClipboardTaskListLtr } from '@styled-icons/fluentui-system-regular/ClipboardTaskListLtr';
 
 function SideBar() {
   return (
@@ -15,6 +16,9 @@ function SideBar() {
       </MenuItem>
       <MenuItem href={'/'}>
         <FlowCascade></FlowCascade>Fluxos
+      </MenuItem>
+      <MenuItem href={'/processes'}>
+        <ClipboardTaskListLtr></ClipboardTaskListLtr>Processos
       </MenuItem>
       <Button background="#DE5353">Sair</Button>
     </Container>
