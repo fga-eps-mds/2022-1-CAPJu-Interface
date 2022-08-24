a1f35dfb3616008b190133f33ce326feba08002c
Hellen159 <202016480@aluno.unb.br>
2022-08-23 23:53:49 -0400

    69

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export const baseURL = 'https://capju-service.herokuapp.com/';
 const api = axios.create({
   baseURL: 'http://localhost:3000/'
-  baseURL: baseURL
 });
 
 export default api;
