856a6753f21516c9331236ed86326a6c9fbe273d
aquiles23 <jjoseaquiless@gmail.com>
2022-08-28 17:44:11 -0300

    68 ordenando processos
    Co-authored-by: luclopesr <luclopesr@gmail.com>

--- src/pages/Processes/index.js
+++ src/pages/Processes/index.js
@@ -183,76 +183,80 @@ function Processes() {
           />
         </div>
         {processes.length == 0 && 'Nenhum processo foi encontrado'}
-        {filterProcesses(processes).map((proc, idx) => {
-          let CurrentStage, FinalStage;
+        {filterProcesses(processes)
+          .sort((a, b) => b.etapas.length - a.etapas.length)
+          .map((proc, idx) => {
+            let CurrentStage, FinalStage;
 
-          if (flow && stages) {
-            CurrentStage = stages.find((el) => el._id === proc.etapaAtual);
-            FinalStage = stages.find(
-              (el) => el._id === flow.sequences.at(-1).to
-            );
-          }
+            if (flow && stages) {
+              CurrentStage = stages.find((el) => el._id === proc.etapaAtual);
+              FinalStage = stages.find(
+                (el) => el._id === flow.sequences.at(-1).to
+              );
+            }
 
-          return (
-            <div key={idx} className="process">
-              <div className="processName">
-                {proc.apelido.length > 0
-                  ? `${proc.registro} - ${proc.apelido}`
-                  : `${proc.registro}`}
-                {
-                  <Link to="showProcess" state={{ proc, flow }}>
-                    <Visibility className="see-process"></Visibility>
-                  </Link>
-                }
-                <EditIcon
-                  className="edit-process"
-                  onClick={() => openEditModal(proc)}
-                />
-                <DeleteForeverIcon
-                  className="delete-process"
-                  onClick={() => openModal()}
-                />
-                <Modal
-                  isOpen={modalIsOpen}
-                  onRequestClose={closeModal}
-                  style={customStyles}
-                  contentLabel="excluir processo"
-                >
-                  <ModalHeader close={closeModal}>Excluir Processo</ModalHeader>
-                  <p>
-                    Tem certeza que deseja excluir o processo {proc.registro}?
-                  </p>
-                  <ModalBody>
-                    <Button
-                      onClick={async () => {
-                        await deleteProcess(proc.registro);
-                        await updateProcesses();
-                        closeModal();
-                      }}
-                    >
-                      Confirmar
-                    </Button>
-                    <Button onClick={closeModal} background="red">
-                      Cancelar
-                    </Button>
-                  </ModalBody>
-                </Modal>
+            return (
+              <div key={idx} className="process">
+                <div className="processName">
+                  {proc.apelido.length > 0
+                    ? `${proc.registro} - ${proc.apelido}`
+                    : `${proc.registro}`}
+                  {
+                    <Link to="showProcess" state={{ proc, flow }}>
+                      <Visibility className="see-process"></Visibility>
+                    </Link>
+                  }
+                  <EditIcon
+                    className="edit-process"
+                    onClick={() => openEditModal(proc)}
+                  />
+                  <DeleteForeverIcon
+                    className="delete-process"
+                    onClick={() => openModal()}
+                  />
+                  <Modal
+                    isOpen={modalIsOpen}
+                    onRequestClose={closeModal}
+                    style={customStyles}
+                    contentLabel="excluir processo"
+                  >
+                    <ModalHeader close={closeModal}>
+                      Excluir Processo
+                    </ModalHeader>
+                    <p>
+                      Tem certeza que deseja excluir o processo {proc.registro}?
+                    </p>
+                    <ModalBody>
+                      <Button
+                        onClick={async () => {
+                          await deleteProcess(proc.registro);
+                          await updateProcesses();
+                          closeModal();
+                        }}
+                      >
+                        Confirmar
+                      </Button>
+                      <Button onClick={closeModal} background="red">
+                        Cancelar
+                      </Button>
+                    </ModalBody>
+                  </Modal>
+                </div>
+                {flow && stages ? (
+                  <>
+                    <div className="processName currentStage">
+                      Epata Atual: {CurrentStage?.name}
+                    </div>
+                    <div className="processName finalStage">
+                      Última Etapa: {FinalStage?.name}
+                    </div>
+                  </>
+                ) : (
+                  ''
+                )}
               </div>
-              {flow && stages ? (
-                <>
-                  <div className="processName currentStage">
-                    Epata Atual: {CurrentStage?.name}
-                  </div>
-                  <div className="processName finalStage">
-                    Última Etapa: {FinalStage?.name}
-                  </div>
-                </>
-              ) : (
-                ''
-              )}
-            </div>
-          );
-        })}
+            );
+          })}
         <Modal
           isOpen={editModalIsOpen}
           onRequestClose={closeModal}
