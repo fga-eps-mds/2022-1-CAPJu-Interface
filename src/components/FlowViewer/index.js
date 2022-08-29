3c006bfa22c9fa45d4c0be9ca2dbd6fc9dec793d
aquiles23 <jjoseaquiless@gmail.com>
2022-08-29 00:44:51 -0300

    68 mostrando se etapa está atrasada
    Co-authored-by: luclopesr <luclopesr@gmail.com>

--- src/components/FlowViewer/index.js
+++ src/components/FlowViewer/index.js
@@ -4,11 +4,23 @@ import { Container } from './styles';
 import ReactFlow, { MarkerType } from 'react-flow-renderer';
 
 function FlowViewer(props) {
-  console.log(
-    props.stages.filter((stage) => {
-      return props.flow.stages.includes(stage._id);
-    })
-  );
+  const procStages = props.proc.etapas;
+  function isLate(stage) {
+    const today = new Date();
+    const dayInMilisseconds = 24 * 3600 * 1000;
+    let stageDate;
+    if (props.highlight === props.flow.sequences[0].from) {
+      stageDate = new Date(props.proc.createdAt);
+    } else {
+      stageDate = new Date(props.proc.etapas.at(-1).createdAt);
+    }
+    const timeInDays = Math.abs(today - stageDate) / dayInMilisseconds;
+    console.warn(timeInDays);
+    if (timeInDays > parseInt(stage.time)) {
+      return true;
+    }
+    return false;
+  }
   const nodes = props.stages
     .filter((stage) => {
       return props.flow.stages.includes(stage._id);
@@ -20,15 +32,18 @@ function FlowViewer(props) {
         position: { x: (idx % 2) * 100, y: 80 * idx },
         style:
           props.highlight == stage._id
-            ? { backgroundColor: '#1b9454', color: '#f1f1f1' }
+            ? {
+                backgroundColor: isLate(stage) ? '#de5353' : '#1b9454',
+                color: '#f1f1f1'
+              }
             : {}
       };
     });
 
   let edges;
-  if (props.procStages) {
+  if (procStages) {
     const edgesProcs =
-      props.procStages.map((sequence) => {
+      procStages.map((sequence) => {
         return {
           id: 'e' + sequence.stageIdFrom + '-' + sequence.stageIdTo,
           source: sequence.stageIdFrom,
@@ -77,7 +92,7 @@ FlowViewer.propTypes = {
   flow: PropTypes.any,
   stages: PropTypes.array,
   highlight: PropTypes.string,
-  procStages: PropTypes.array
+  proc: PropTypes.object
 };
 
 export default FlowViewer;
--- src/pages/ShowProcess/index.js
+++ src/pages/ShowProcess/index.js
@@ -12,6 +12,8 @@ import ModalHeader from 'components/ModalHeader';
 import ModalBody from 'components/ModalBody';
 import { Ring } from 'react-awesome-spinners';
 
+Modal.setAppElement('#root');
+
 const customStyles = {
   content: {
     top: '30%',
@@ -136,7 +138,7 @@ function ShowProcess() {
               stages={stages}
               flow={flow}
               highlight={proc.etapaAtual}
-              procStages={proc.etapas}
+              proc={proc}
             ></FlowViewer>
           </FlowWrapper>
         ) : (
