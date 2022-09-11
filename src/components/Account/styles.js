b8474f59b1ce337f46af81b5be9a1700a06f5499
Luciano Machado <luciano.asb.machado@gmail.com>
2022-09-11 18:47:29 -0300

    72 - Adicionado o componente Account e a sua imagem de botao
    Co-authored-by: Luciano <luciano.abs.machado@gmail.com>
    Co-authored-by: Hellen <202016480@aluno.unb.br>
    Co-authored-by: Francisco Heronildo <heronildo.2010@hotmail.com>

---
+++ src/components/Account/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { UserCircle } from '@styled-icons/boxicons-regular/UserCircle';
+
+export const User = styled(UserCircle)`
+  color: #304974;
+  position: absolute;
+  top: 1%;
+  right: 5%;
+  height: 10%;
+  width: 10%;
+`;
